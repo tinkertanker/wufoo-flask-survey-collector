12eea2a26514aa881a695ef1c547396bf11ff130
Junwei Low <lowjunwei99@gmail.com>
2020-04-28 11:55:38 +0800

    added list of students not submitted for each lesson

--- app.py
+++ app.py
@@ -115,8 +115,14 @@ def lesson_page():
                 responses_problems.append(i)
         cursor = db.execute("SELECT additional_qn,student_index FROM surveys WHERE survey = ?",(survey_lesson,))
         responses_additional = cursor.fetchall()
+        missing = []
+        for i in range(1,59):
+            cursor = db.execute("SELECT * FROM surveys WHERE survey = ? AND student_index = ?",(survey_lesson,i))
+            check = cursor.fetchone()
+            if check == None:
+                missing.append(i)
         db.close()
-        return render_template("lesson.html", lesson_number=lesson_number, order=order,color=color, survey_lesson=survey_lesson, responses_follow=responses_follow, responses_complete=responses_complete, responses_pace=responses_pace, responses_problems=responses_problems, responses_additional=responses_additional)
+        return render_template("lesson.html", lesson_number=lesson_number, missing=missing, order=order,color=color, survey_lesson=survey_lesson, responses_follow=responses_follow, responses_complete=responses_complete, responses_pace=responses_pace, responses_problems=responses_problems, responses_additional=responses_additional)
     return render_template("lesson.html")
 
 @app.route("/student", methods=["GET","POST"])
